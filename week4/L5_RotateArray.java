951601fad8dd56727ba46725af0d1edfe877977b
Advika-Singh <phoenixada2118@gmail.com>
2026-09-12 16:32:41 +0530

    week 4

---
+++ week4/L5_RotateArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class L5_RotateArray {
+
+    static int[] rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[(i + k) % n] = nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println(Arrays.toString(rotateArray(new int[]{1, 2}, 3)));
+    }
+}
